2e28a11104d4cf5528c6035c2062382cb880e1ac
Max Murphy <murphymax1@gmail.com>
2022-05-10 13:10:47 -0400

    added image page render

---
+++ memory-game/src/data.js
@@ -0,0 +1,12 @@
+export const imgArr = [
+    'memory-game/public/images/matt-nelson-bx4WS8mhz3w-unsplash.jpg',
+    './images/nasa--5V6VZxSQRo-unsplash.jpg',
+    './images/nasa-2W-QWAC0mzI-unsplash.jpg',
+    './images/nasa-71W3CWeZF7A-unsplash.jpg',
+    './images/nasa-JHyiw_dpALk-unsplash.jpg',
+    './images/nasa-Li41RApUAQA-unsplash.jpg',
+    './images/nasa-m35LirqP6y8-unsplash.jpg',
+    './images/nasa-rTZW4f02zY8-unsplash.jpg'
+]
+
+
