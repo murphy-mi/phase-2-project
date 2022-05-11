dba992c1bebf22539b7c1e53052d362a87cae191
Max Murphy <murphymax1@gmail.com>
2022-05-11 10:38:58 -0400

    Set up flip functionality

--- memory-game/src/components/GamePage.js
+++ memory-game/src/components/GamePage.js
@@ -9,8 +9,10 @@ function GamePage() {
 
     const [imagesArray, setImagesArray] = useState([])
     const [cardsChosen, setCardsChosen] = useState([])
-    const [cardsChosenIds, setCardsChosenIds] = useState([])
+    const [cardsChosenIdx, setCardsChosenIdx] = useState([])
     const [points, setPoints] = useState(0)
+    const [openCards, setOpenCards] = useState([])
+    // const [isFlipped, setIsFlipped] = useState(false)
 
 
     function createCardBoard() {
@@ -18,6 +20,39 @@ function GamePage() {
         const shuffledArray = shuffleArray(imagesGenerated)
         setImagesArray(shuffledArray)
     }
+
+
+    function handleFlip(image, index) {
+        // console.log(image, index)
+
+        if (cardsChosenIdx.length === 1 && cardsChosenIdx[0] === index) {
+            return
+        }
+
+        if (cardsChosen.length < 2) {
+            setCardsChosen(cardsChosen => cardsChosen.concat(image))
+            setCardsChosenIdx(cardsChosenIdx => cardsChosenIdx.concat(index))
+            // console.log(cardsChosen)
+            // console.log(cardsChosenIdx)
+
+            if (cardsChosen.length === 1) {
+                if (cardsChosen[0] === image) {
+                    setPoints(pointes => points + 2)
+                    setOpenCards(openCards => openCards.concat([cardsChosen[0], image]))
+                }
+                setTimeout(() => {
+                    setCardsChosenIdx([])
+                    setCardsChosen([])
+                }, 700)
+            }
+        }
+    }
+
+    function isCardChosen(image, index) {
+        return cardsChosenIdx.includes(index) || openCards.includes(image)
+    }
+
+
 
     function shuffleArray(array) {
         for (let i = 0; i < array.length; i++) {
@@ -37,7 +72,7 @@ function GamePage() {
             <Routes>
                 <Route
                     path="/"
-                    element={<GamePlayer imagesArray={imagesArray} />}
+                    element={<GamePlayer imagesArray={imagesArray} handleFlip={handleFlip} points={points} isCardChosen={isCardChosen} />}
                 />
                 <Route
                     path="/highscores"
--- memory-game/src/components/GamePlayer.js
+++ memory-game/src/components/GamePlayer.js
@@ -4,22 +4,23 @@ import ScoreForm from './ScoreForm';
 import GamePlayerCard from './GamePlayerCard'
 
 
-function GamePlayer({ imagesArray }) {
+function GamePlayer({ imagesArray, handleFlip, points, isCardChosen }) {
     console.log(imagesArray)
 
     const gameCard = imagesArray.map((image, index) => {
         return (
-            <GamePlayerCard key={index} image={image} />
+            <GamePlayerCard key={index} image={image} isCardChosen={isCardChosen} index={index} handleFlip={handleFlip} />
         )
     })
 
     return (
         <div className="GamePlayer">
             <ScoreForm />
-             <div className='cards-wrapper'>
+            <div className='cards-wrapper'>
                 {gameCard}
+                <h1 style={{ color: 'cyan' }} > Score:{points}</h1>
             </div>
-        </div>
+        </div >
     );
 }
 
