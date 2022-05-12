bbcb9b58f37e5f7a94d04e8f65bf1e3c13d6aab2
Kaitlyn Greve <kgreve14@gmail.com>
2022-05-12 14:22:23 -0400

    highscores styling

--- memory-game/src/components/HighScores.js
+++ memory-game/src/components/HighScores.js
@@ -5,22 +5,24 @@ function HighScores({ scores }) {
 
 
     return (
-        <table className="HighScores">
-            <tr>
-                <th>
-                    Name
-                </th>
-                <th>
-                    Time (s)
-                </th>
-                <th>
-                    Guesses
-                </th>
-            </tr>
-            {scores.map((score) => {
-                return <HighScoresCard score={score} />
-            })}
-        </table>
+        <div className="high-scores-container">
+            <table className="HighScores">
+                <tr>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Time (s)
+                    </th>
+                    <th>
+                        Guesses
+                    </th>
+                </tr>
+                {scores.map((score) => {
+                    return <HighScoresCard score={score} />
+                })}
+            </table>
+        </div>
     );
 }
 
